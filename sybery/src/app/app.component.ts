a02ad16b80222a53b8b7102e7f1c50cf53384e10
Swadesh <swadeshrout2@gmail.com>
2020-07-28 11:51:35 +0530

    navbar has been added

--- sybery/src/app/app.component.ts
+++ sybery/src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  altLogo = 'sybery';
+export class AppComponent implements OnInit {
+  public altLogo = 'sybery';
+  public isHomeActive = true;
+  public isServiceActive = false;
+  public isPortfolioActive = false;
+  public isAboutActive = false;
+  public isContactActive = false;
+
+  ngOnInit() {
+
+  }
+
+  onSelectNav(navType) {
+
+    if (navType == 'Home') {
+      this.isHomeActive = true;
+      this.isServiceActive = false;
+      this.isPortfolioActive = false;
+      this.isAboutActive = false;
+      this.isContactActive = false;
+
+    } else if (navType == 'Service') {
+      this.isServiceActive = true;
+      this.isHomeActive = false;
+      this.isPortfolioActive = false;
+      this.isAboutActive = false;
+      this.isContactActive = false;
+
+    } else if (navType == 'Portfolio') {
+      this.isPortfolioActive = true;
+      this.isServiceActive = false;
+      this.isHomeActive = false;
+      this.isAboutActive = false;
+      this.isContactActive = false;
+
+    } else if (navType == 'About') {
+      this.isAboutActive = true;
+      this.isPortfolioActive = false;
+      this.isServiceActive = false;
+      this.isHomeActive = false;
+      this.isContactActive = false;
+
+    } else if (navType == 'Contact') {
+      this.isContactActive = true;
+      this.isAboutActive = false;
+      this.isPortfolioActive = false;
+      this.isServiceActive = false;
+      this.isHomeActive = false;
+
+    }
+  }
 }
